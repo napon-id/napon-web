fcd82c04426012f6a1929ea921844f5f96666bef
Norfa Bagas <bgsnorfa@gmail.com>
2019-01-19 19:55:56 +0700

    refactor env

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -5,11 +5,11 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>{{ config('app.name') }}</title>
+  <title>{{ config('app.name') }} - Platform Menabung Pohon</title>
   @include('includes.css')
 
   @yield('style')
-  
+
 </head>
 
 <body>
