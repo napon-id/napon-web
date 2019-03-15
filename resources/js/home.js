c53c5a7b583e38492a1fa9d99920085d7212637f
nb <bgsnorfa@gmail.com>
2019-03-15 09:51:29 +0700

    add lightbox to service products

--- resources/js/home.js
+++ resources/js/home.js
@@ -7,7 +7,11 @@ require('mdbootstrap/js/mdb.min');
 // slick carousel
 require('slick-carousel/slick/slick.min');
 
+// lightbox
+require('ekko-lightbox/ekko-lightbox');
+
 // components
 require('./home/slick');
 require('./home/scroll');
 require('./home/faq');
+require('./home/lightbox');
