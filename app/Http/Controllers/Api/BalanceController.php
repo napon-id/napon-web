0a468aa977a456cbf36eee04d43829b75f9dc45f
bgsnorfa <bgsnorfa@gmail.com>
2019-07-04 20:57:01 +0700

    Add withdraw endpoint functionalities

--- app/Http/Controllers/Api/BalanceController.php
+++ app/Http/Controllers/Api/BalanceController.php
@@ -10,6 +10,7 @@ use App\Topup;
 use App\Withdraw;
 use App\Http\Controllers\Traits\Firebase;
 use App\Http\Controllers\Traits\MidTrans;
+use App\Account;
 
 class BalanceController extends Controller
 {
@@ -96,6 +97,74 @@ class BalanceController extends Controller
      */
     public function withdraw(Request $request)
     {
+        if ($request->has('user_key') && $request->user_key != '') {
+            $email = $this->getUserEmail((string) $request->user_key);
 
+            if ($email == '') {
+                return response()->json([
+                    'request_code' => 200,
+                    'result_code' => 2, 
+                    'message' => 'User not found'
+                ]);
+            } else {
+                $user = User::where('email', $email)->first();
+
+                if ($request->has('withdraw_amount') && $request->withdraw_amount != '') {
+                    if (is_numeric($request->withdraw_amount) && $request->withdraw_amount >= 10000 && $request->withdraw_amount <= $user->balance->balance) {
+                        
+                        if ($request->has('user_bank_id') && $request->user_bank_id != '') {
+                            $bank = Account::where('token', $request->user_bank_id)->first();
+
+                            if (isset($bank)) {
+                                $withdraw = Withdraw::create([
+                                    'user_id' => $user->id,
+                                    'token' => 'Withdraw-' . now(),
+                                    'account_id' => $bank->id,
+                                    'amount' => $request->withdraw_amount
+                                ]);
+
+                                if ($withdraw) {
+                                    return response()->json([
+                                        'request_code' => 200,
+                                        'result_code' => 4,
+                                        'message' => 'Withdraw processed'
+                                    ]);
+                                }
+                            } else {
+                                return response()->json([
+                                    'request_code' => 200,
+                                    'result_code' => 7,
+                                    'message' => 'Bad request'
+                                ]);
+                            }
+                        } else {
+                            return response()->json([
+                                'request_code' => 200,
+                                'result_code' => 9,
+                                'message' => 'There is no data'
+                            ]);
+                        }
+                    } else {
+                        return response()->json([
+                            'request_code' => 200,
+                            'result_code' => 7,
+                            'message' => 'Bad request'
+                        ]);
+                    }   
+                } else {
+                    return response()->json([
+                        'request_code' => 200,
+                        'result_code' => 9,
+                        'message' => 'There is no data'
+                    ]);
+                }
+            }
+        } else {
+            return response()->json([
+                'request_code' => 200,
+                'result_code' => 2,
+                'message' => 'User not found'
+            ]);
+        }
     }
 }
