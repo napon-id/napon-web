23e0b9cd8e54cb85e98a1faea61e9557319cdde5
bgsnorfa <bgsnorfa@gmail.com>
2019-07-13 14:43:19 +0700

    Fix term_and_condition and contact field validation

--- resources/views/admin/setting/contact.blade.php
+++ resources/views/admin/setting/contact.blade.php
@@ -22,22 +22,42 @@
             @csrf
             <div class="form-group">
                 <label for="address">{{ __('Address') }}</label>
-                <textarea name="address" class="form-control">{{ $address->value }}</textarea>
+                <textarea name="address" class="form-control {{ $errors->has('address') ? 'is-invalid' : '' }}">{{ $address->value }}</textarea>
+                @if ($errors->has('address'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('address') }}</strong>
+                </span>
+                @endif
             </div>
 
             <div class="form-group">
                 <label for="email">{{ __('Email') }}</label>
-                <input type="text" name="email" value="{{ $email->value }}" class="form-control">
+                <input type="text" name="email" value="{{ $email->value }}" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}">
+                @if ($errors->has('email'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+                @endif
             </div>
 
             <div class="form-group">
                 <label for="phone">{{ __('Phone') }}</label>
-                <input type="text" name="phone" value="{{ $phone->value }}" class="form-control">
+                <input type="text" name="phone" value="{{ $phone->value }}" class="form-control {{ $errors->has('phone') ? 'is-invalid' : '' }}">
+                @if ($errors->has('phone'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('phone') }}</strong>
+                </span>
+                @endif
             </div>
 
             <div class="form-group">
                 <label for="website">{{ __('Website') }}</label>
-                <input type="text" name="website" value="{{ $website->value }}" class="form-control">
+                <input type="text" name="website" value="{{ $website->value }}" class="form-control {{ $errors->has('website') ? 'is-invalid' : '' }}">
+                @if ($errors->has('website'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('website') }}</strong>
+                </span>
+                @endif
             </div>
 
             <input type="submit" name="update" value="Update" class="btn btn-info">
